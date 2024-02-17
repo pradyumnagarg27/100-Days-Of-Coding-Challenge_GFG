14abfd67725cb0ac500c834e2f408d6012e93668
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-17 20:47:12 +0530

    Create GFG.java

---
+++ 17-02-24/Sum of Series/GFG.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            long res = obj.seriesSum(n);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static long seriesSum(int n) {
+        return (long)((long)n*(long)(n+1)/2);
+    }
+}
+        

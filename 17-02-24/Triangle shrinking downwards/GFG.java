0feffec88e1bdd18c4ea13ea85c981bfdbd31947
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-17 21:47:16 +0530

    Create GFG.java

---
+++ 17-02-24/Triangle shrinking downwards/GFG.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+
+            Solution ob = new Solution();
+            
+            String ans = ob.triDownwards(S);
+            
+            for(int i=0 ; i<ans.length() ; i++)
+            {
+                System.out.print(ans.charAt(i));
+                if((i+1)%S.length()==0)
+                    System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static String triDownwards(String S){
+        String r=S;
+        for(int i=1;i<S.length();i++){
+            r=r+".".repeat(i)+S.substring(i);
+        }
+        return r;
+    }
+};

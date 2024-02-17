77aa1680cb77aa587cfd0adbf23e710da0a2936d
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-17 22:43:14 +0530

    Create PrintPattern.java

---
+++ 17-02-24/Print the pattern | Set-1/PrintPattern.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+
+
+
+class PrintPattern
+{
+	public  static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			GfG g = new GfG();
+			g.printPat(n);
+			System.out.println();
+		
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class GfG
+{
+    void printPat(int n)
+    {
+         for(int i=0;i<n;i++){
+             for(int j=n;j>0;j--){
+                 for(int k=0;k<n-i;k++){
+                     System.out.print(j+" ");
+                 }
+             }
+             System.out.print("$");
+         }
+    }
+}

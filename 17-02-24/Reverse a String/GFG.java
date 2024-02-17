10858c040370ce5d3070409fed7f9c97a3162ff2
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-17 22:26:13 +0530

    Create GFG.java

---
+++ 17-02-24/Reverse a String/GFG.java
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+// Initial template for Java
+
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            Solution ob = new Solution();
+
+            System.out.println(ob.revStr(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static String revStr(String S) {
+        String r="";
+        for(int i=0;i<S.length();i++){
+            r+=S.charAt(S.length()-i-1);
+        }
+        return r;
+    }
+}

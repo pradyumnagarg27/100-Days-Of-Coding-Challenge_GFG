5cf5e78cac50694d3ebf423309d931f2571a9184
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-18 20:24:38 +0530

    Create GFG.java

---
+++ 18-02-24/Reverse digits/GFG.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());            
+            Solution ob = new Solution();
+            long ans = ob.reverse_digit(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long reverse_digit(long n)
+    {
+        String s=String.valueOf(n);
+        String x="";
+        for(int i=0;i<s.length();i++){
+            x+=s.charAt(s.length()-i-1);
+        }
+        return Long.valueOf(x);
+    }
+}

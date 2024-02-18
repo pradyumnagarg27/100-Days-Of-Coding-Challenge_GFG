8b430bbd01c7468c8105c5e2dcac624fcf19dccf
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-18 21:52:06 +0530

    Create GFG.java

---
+++ 18-02-24/Sum Of Digits/GFG.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.sumOfDigits(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int sumOfDigits(int N) {
+        int s=0;
+        while(N>=10){
+            s+=N%10;
+            N/=10;
+        }
+        s+=N;
+        return s;
+    }
+}
+       

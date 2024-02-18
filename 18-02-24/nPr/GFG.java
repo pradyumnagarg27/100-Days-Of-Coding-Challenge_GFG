172dab4da11a355e9ce511ae6d8e65753ca04a2c
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-18 19:52:27 +0530

    Create GFG.java

---
+++ 18-02-24/nPr/GFG.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String a[] = in.readLine().trim().split("\\s+");
+            Long n = Long.parseLong(a[0]);
+            Long r = Long.parseLong(a[1]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.nPr(n, r));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    long fact(long n){
+        if(n==0){
+            return 1;
+        }else{
+            return(n*fact(n-1));
+        }
+    }
+    long nPr(long n, long r){
+        return fact(n)/fact(n-r);
+    }
+}

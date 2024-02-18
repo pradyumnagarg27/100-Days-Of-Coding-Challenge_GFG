4ee961edb3bb8ecf4c92eac1196b9726afde0588
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-18 20:19:13 +0530

    Create GFG.java

---
+++ 18-02-24/Palindrome/GFG.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            String ans = ob.is_palindrome(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String is_palindrome(int n)
+    {
+        String s=Integer.toString(n);
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i)!=s.charAt(s.length()-i-1)){
+                return "No";
+            }
+        }
+        return "Yes";
+    }
+}

343dafbf2aa10d3628e036046b70894729e04b1b
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-18 22:03:20 +0530

    Create GfG.java

---
+++ 18-02-24/C++ Strings/GfG.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String S1 = sc.next();
+                    String S2 = sc.next();
+                    Sol obj = new Sol();
+                    System.out.println(obj.conCat(S1,S2));
+                }
+                
+        }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+   String conCat(String s1 , String s2)
+   {
+       return s1+s2;
+   }
+}

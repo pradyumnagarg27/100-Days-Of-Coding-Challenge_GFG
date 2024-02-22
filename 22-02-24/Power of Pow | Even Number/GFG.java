7b52a87b48f6eef7b8fc061d3b785fb825017f9d
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-22 07:41:54 +0530

    Create GFG.java

---
+++ 22-02-24/Power of Pow | Even Number/GFG.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());            
+            Solution ob = new Solution();
+            long ans = ob.sum_of_square_evenNumbers(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long sum_of_square_evenNumbers(long n)
+    {
+        long s=0;
+        for(long i=2;i<=2*n;i+=2){
+            s+=i*i;
+        }
+        return s;
+    }
+}

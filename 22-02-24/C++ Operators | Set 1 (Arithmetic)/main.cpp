89e7bdec74c31fa33080f4638799e69773c3ba4d
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-22 06:59:38 +0530

    Create main.cpp

---
+++ 22-02-24/C++ Operators | Set 1 (Arithmetic)/main.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> cppOperators(int A, int B) {
+        vector<int> v;
+        if(B>A){
+            v={A+B,A*B,B-A,B/A};
+        } else {
+            v={A+B,A*B,A-B,A/B};
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int A, B;
+        cin >> A >> B;
+        Solution ob;
+        vector<int> ans = ob.cppOperators(A, B);
+        for (int u : ans) cout << u << "\n";
+    }
+}
+// } Driver Code Ends

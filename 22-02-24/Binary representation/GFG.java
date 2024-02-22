c944423fa7b5f664419a18a9665b50269c6dca00
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-22 07:39:06 +0530

    Create GFG.java

---
+++ 22-02-24/Binary representation/GFG.java
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N = sc.nextInt();
+
+            Solution ob = new Solution();
+            String ans = ob.getBinaryRep(N);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static String getBinaryRep(int N){
+        String r=Integer.toBinaryString(N);
+        String x="0".repeat(30-r.length())+r;
+        return x;
+    }
+}

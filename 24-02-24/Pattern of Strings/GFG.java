b1b8100cec88f4d99f5352978c644aa54ab360e5
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-24 20:42:29 +0530

    Create GFG.java

---
+++ 24-02-24/Pattern of Strings/GFG.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            
+            Solution ob = new Solution();
+            ArrayList<String> result = ob.pattern(S);
+            for(String value : result){
+                System.out.println(value);
+            }
+            
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    ArrayList<String> pattern(String S) {
+        ArrayList<String> r=new ArrayList<String>();
+        int n=S.length();
+        for(int i=0;i<n;i++){
+            r.add(S.substring(0,n-i));
+        }
+        return r;
+    }
+};

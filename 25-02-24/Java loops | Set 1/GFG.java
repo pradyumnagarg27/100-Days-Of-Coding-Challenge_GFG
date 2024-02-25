a11c803dfe8158188a8f6dfd6118354ed5c0a6b3
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-25 10:06:04 +0530

    Create GFG.java

---
+++ 25-02-24/Java loops | Set 1/GFG.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0)
+        {
+            int N = scn.nextInt();
+
+            Solution ob = new Solution();
+            ArrayList<Integer> sum = ob.getSum(N);
+            System.out.println(sum.get(0)+" "+sum.get(1));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static ArrayList<Integer> getSum(int N){
+        ArrayList<Integer> r=new ArrayList<Integer>(2);
+        int e=0,o=0;
+        for(int i=1;i<=N;i++){
+            if(i%2==0){
+                e+=i;
+            }else{
+                o+=i;
+            }
+        }
+        r.add(e);
+        r.add(o);
+        return r;
+    }
+}

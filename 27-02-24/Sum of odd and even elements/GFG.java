03df06cbb0a73423186835dc10ef33864936df66
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-27 15:16:06 +0530

    Create GFG.java

---
+++ 27-02-24/Sum of odd and even elements/GFG.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int[] ans = ob.find_sum(n);
+            for(int i = 0; i < ans.length; i++)
+            {
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] find_sum(int n)
+    {
+        int o=0,e=0;
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+                e+=i;
+            }else{
+                o+=i;
+            }
+        }
+        int[] r={o,e};
+        return r;
+    }
+}

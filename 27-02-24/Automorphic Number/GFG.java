53be0f1d62cf9c41ab685d1881c2976552ae9df5
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-27 15:58:20 +0530

    Create GFG.java

---
+++ 27-02-24/Automorphic Number/GFG.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            String ans = ob.is_AutomorphicNumber(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String is_AutomorphicNumber(int n)
+    {
+        if(n%10==(n*n)%10){
+            return "Automorphic";
+        }else{
+            return "Not Automorphic";
+        }
+    }
+}

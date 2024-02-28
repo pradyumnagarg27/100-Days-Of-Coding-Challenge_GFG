7bc71f63dec0f26c7dadcec59d1d6c59a00fcb12
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-28 19:50:45 +0530

    Create GFG.java

---
+++ 28-02-24/Addition of two square matrices/GFG.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] matrixA = new int[n][n];
+            int[][] matrixB = new int[n][n];
+            for(int i = 0; i < n; i++){
+                String[] S = br.readLine().trim().split(" ");
+                for(int j = 0; j < n; j++)
+                    matrixA[i][j] = Integer.parseInt(S[j]);
+            }
+            for(int i = 0; i < n; i++){
+                String[] S = br.readLine().trim().split(" ");
+                for(int j = 0; j < n; j++)
+                    matrixB[i][j] = Integer.parseInt(S[j]);
+            }
+            Solution ob = new Solution();
+            ob.Addition(matrixA, matrixB);
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    System.out.print(matrixA[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public void  Addition(int[][] A, int[][] B)
+    {
+        int n=A.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                A[i][j]+=B[i][j];
+            }
+        }
+    }
+}

c6398f2543d59e7f221c07563a5cb5c70fa5b7bd
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-02-28 20:02:29 +0530

    Create GfG.java

---
+++ 28-02-24/Number of Diagonals/GfG.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int N = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.diagonals(N));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long diagonals(int n) 
+    { 
+       if(n<4){
+           return 0;
+       }
+       long r=(long)n*(n-3)/2;
+       return r;
+    } 
+}

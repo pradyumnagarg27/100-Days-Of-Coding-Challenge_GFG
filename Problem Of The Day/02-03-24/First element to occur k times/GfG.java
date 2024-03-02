f22b1697490d7f4bb39dc444e0bf726d2a941df0
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-03-02 17:08:41 +0530

    Create GfG.java

---
+++ Problem Of The Day/02-03-24/First element to occur k times/GfG.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class GfG { 
+	public static void main (String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int k = Integer.parseInt(inputLine[1]);
+		    int[] arr = new int[n];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++)arr[i] = Integer.parseInt(inputLine[i]);
+		    
+		    Solution obj = new Solution();
+		    System.out.println(obj.firstElementKTime(n, k, arr));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public int firstElementKTime(int n, int k, int[] a) { 
+        HashMap<Integer, Integer> c = new HashMap<>();
+        for (int i = 0; i < n; i++)
+        {
+            c.put(a[i], c.getOrDefault(a[i], 0) + 1);
+            if (c.get(a[i]) == k) 
+            {
+                return a[i];
+            }
+        }
+ 
+        return -1;
+    }
+}

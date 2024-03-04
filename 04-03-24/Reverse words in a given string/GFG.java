31a6404c0ddd0d0ac45540370576f4c8ece70b43
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-03-04 21:19:28 +0530

    Create GFG.java

---
+++ 04-03-24/Reverse words in a given string/GFG.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.reverseWords(s));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution 
+{
+    //Function to reverse words in a given string.
+    String reverseWords(String S)
+    {
+        String[] arr=S.split("[.]");
+        Collections.reverse(Arrays.asList(arr));
+        String str = String.join(".", arr);
+        return str;
+    }
+}

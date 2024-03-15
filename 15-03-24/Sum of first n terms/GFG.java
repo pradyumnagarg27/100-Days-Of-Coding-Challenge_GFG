8c82c5ae766a6812cb81b5735a175096bd88ae30
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-03-15 22:38:26 +0530

    Create GFG.java

---
+++ 15-03-24/Sum of first n terms/GFG.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.sumOfSeries(N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    long sumOfSeries(long n) {
+        // code here
+        long r=0;
+        for(long i=1;i<n+1;i++){
+            r+=(i*i*i);
+        }
+        return r;
+    }
+}

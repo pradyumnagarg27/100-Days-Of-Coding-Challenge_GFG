6d5fc1875f84799d740dd7a52885101838aadd41
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-03-15 22:35:49 +0530

    Create GFG.java

---
+++ Problem Of The Day/15-03-24/Linked List that is Sorted Alternatingly/GFG.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class GFG {
+    static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	public static void main (String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    
+		    Node head = new Node(sc.nextInt());
+		    Node tail = head;
+		    
+		    for(int i=1; i<n; i++){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		    Solution obj = new Solution();
+		    head = obj.sort(head);
+		    printList(head);
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+
+/*
+class Node {
+    int data;
+    Node next;
+    
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    
+   public Node sort(Node head){
+        //your code here, return the head of the sorted list
+        if(head == null && head.next == null){
+            return head;
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        Node current = head;
+        while(current != null){
+            list.add(current.data);
+            current = current.next;
+        }
+        Collections.sort(list);
+        
+        current = head; 
+        for(int i=0;i<list.size();i++){
+            current.data = list.get(i);
+            current = current.next;
+        }
+        return head;
+   }
+
+
+}
